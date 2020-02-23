75b34af8e751f2f38b4b302f2d9d4fc15980c329
sD <stoned@derpymail.org>
2020-02-23 18:16:08 +0100

    add char export script

---
+++ export_charnames.ps1
@@ -0,0 +1,10 @@
+﻿'export default [' | Out-File -encoding ASCII -FilePath "E:\Git\webAO\webAO\characters.js"
+$charfolders = Get-ChildItem -Directory | %{
+$inifile = $_.FullName + '\char.ini'
+if (Test-Path $inifile) {
+    Write-Host $_.Name
+    '    "' + $_.Name + '",' | Add-Content -Path "E:\Git\webAO\webAO\characters.js"
+    }
+}
+
+'];' | Add-Content -Path "E:\Git\webAO\webAO\characters.js"
