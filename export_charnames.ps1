22a90f500affdec0faba13c8ed0d955499e99707
sD <stoned@derpymail.org>
2020-03-07 17:22:30 +0100

    check for charicon too

--- export_charnames.ps1
+++ export_charnames.ps1
@@ -1,7 +1,8 @@
 ﻿'export default [' | Out-File -encoding UTF8 -FilePath "E:\Git\webAO\webAO\characters.js"
 $charfolders = Get-ChildItem -Directory | %{
-$inifile = $_.FullName + '\char.ini'
-if (Test-Path $inifile) {
+$inifile  = $_.FullName + '\char.ini'
+$charicon = $_.FullName + '\char_icon.png'
+if ((Test-Path $inifile) -and (Test-Path $charicon)) {
     Write-Host $_.Name
     '    "' + $_.Name + '",' | Add-Content -Path "E:\Git\webAO\webAO\characters.js"
     }
