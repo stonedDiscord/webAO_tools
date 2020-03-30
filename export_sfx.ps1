0bc73548ef1b99278d601895efe3847e3a4e7ab9
sD <stoned@derpymail.org>
2020-03-30 22:22:18 +0200

    add a script for the sfx

---
+++ export_sfx.ps1
@@ -0,0 +1,9 @@
+﻿'export default [' | Out-File -encoding UTF8 -FilePath "E:\Git\webAO\webAO\sounds.js"
+$charfolders = Get-ChildItem -File | %{
+if( $_.Name -like "sfx-*.wav") {
+    Write-Host $_.Name
+    '    "' + $_.Name + '",' | Add-Content -Path "E:\Git\webAO\webAO\sounds.js"
+    }
+}
+
+'];' | Add-Content -Path "E:\Git\webAO\webAO\sounds.js"
