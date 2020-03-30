9b2a0deec95ab75a4ad7792993c4d7e1e481ae4f
sD <stoned@derpymail.org>
2020-03-30 22:22:40 +0200

    capitalize the first letter to make iniedit work better

--- export_charnames.ps1
+++ export_charnames.ps1
@@ -1,10 +1,12 @@
-﻿'export default [' | Out-File -encoding UTF8 -FilePath "E:\Git\webAO\webAO\characters.js"
+﻿$TI = (Get-Culture).TextInfo
+
+'export default [' | Out-File -encoding UTF8 -FilePath "E:\Git\webAO\webAO\characters.js"
 $charfolders = Get-ChildItem -Directory | %{
 $inifile  = $_.FullName + '\char.ini'
 $charicon = $_.FullName + '\char_icon.png'
 if (Test-Path $inifile) {
-    Write-Host $_.Name
-    '    "' + $_.Name + '",' | Add-Content -Path "E:\Git\webAO\webAO\characters.js"
+    Write-Host $TI.ToTitleCase($_.Name)
+    '    "' + $TI.ToTitleCase($_.Name) + '",' | Add-Content -Path "E:\Git\webAO\webAO\characters.js"
     }
 }
 
