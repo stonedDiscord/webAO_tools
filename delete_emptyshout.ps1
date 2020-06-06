6f02f6f5fd379eabc00d7b33e8fc9d6abf9033cd
sD <stoned@derpymail.org>
2020-06-06 17:21:45 +0200

    Create delete_emptyshout.ps1

---
+++ delete_emptyshout.ps1
@@ -0,0 +1,15 @@
+﻿$BaseFolder = "E:\Git\vanilla-assets\base"
+
+Get-ChildItem -recurse $BaseFolder -Force | % {
+    if (!($_.PSIsContainer) ) {
+        if( $_.Name -like "*.wav") {
+            Write-Host $_.FullName
+            # Delete the swoosh sound without any objection
+            $wavhash = Get-FileHash $_.FullName
+            if ($wavhash.Hash -eq 'E4D5B469F2952137C73A85580175552DEBEE829C6EF41267AF0F1A119883B69B') {
+                Write-Host "deleted with hash"
+                Remove-Item $_.FullName
+            }
+        }
+    }
+}
