ffea94afb845c6268dfa0089dc2020dea171fdab
stonedDiscord <Tukz@gmx.de>
2023-08-10 19:06:56 +0200

    lists

---
+++ backgrounds.ps1
@@ -0,0 +1,11 @@
+﻿$BaseFolder       = "F:\Attorney Online\2.10 content\"
+
+Set-Location ($BaseFolder + '\background\')
+
+'[' | Out-File -encoding UTF8 -FilePath $BaseFolder\backgrounds.json
+$bgfolders = Get-ChildItem -File | %{
+    Write-Host $_.Name
+    '    "' + $_.Name + '",' | Add-Content -Path $BaseFolder\backgrounds.json
+}
+
+'];' | Add-Content -Path $BaseFolder + "\backgrounds.js"
---
+++ chars.ps1
@@ -0,0 +1,13 @@
+﻿$BaseFolder       = "D:\Attorney Online\Attorney Online supermerge webAO edition\base\"
+
+Set-Location ($BaseFolder + '\characters\')
+
+'[' | Out-File -encoding UTF8 -FilePath $BaseFolder\characters.json
+$bgfolders = Get-ChildItem -Directory | %{
+    if (Test-Path -Path (Join-Path $_.Name char.ini)) {
+        Write-Host $_.Name
+        '    "' + $_.Name + '",' | Add-Content -Path $BaseFolder\characters.json
+    }
+}
+
+'];' | Add-Content -Path $BaseFolder\characters.json
